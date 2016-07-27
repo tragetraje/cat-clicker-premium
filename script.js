b9ae6e8548304d9148188be0552383db3335f2c4
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-07-27 21:10:21 +1000

    First commit

---
+++ script.js
@@ -0,0 +1,67 @@
+var Barsik = {
+  name: "Barsik",
+  img: "http://www.catsofaustralia.com/images/freaked_out_kitten.jpg",
+  count: 0
+};
+
+var Murka = {
+  name: "Murka",
+  img: "http://www.catsofaustralia.com/images/kitten_10.jpg",
+  count: 0
+};
+var Kuzya = {
+  name: "Kuzya",
+  img: "http://www.catsofaustralia.com/images/cute_baby_kitten.jpg",
+  count: 0
+};
+var Yashka = {
+  name: "Yashka",
+  img: "http://www.catsofaustralia.com/images/scary_kitten.jpg",
+  count: 0
+};
+var Dymka = {
+  name: "Dymka",
+  img: "http://www.catsofaustralia.com/images/monti_relaxed.jpg",
+  count: 0
+};
+
+var catArray = [Barsik, Murka, Kuzya, Yashka, Dymka];
+
+for (var i = 0; i < catArray.length; i++) {
+
+  //Current number
+  var cat = catArray[i];
+
+  //Create DOM element for the number
+  var elem = document.createElement('div');
+  elem.id = cat.name;
+  elem.className = 'catContainer';
+
+  var list = document.createElement('li');
+  list.textContent = cat.name;
+
+
+  var counter = document.createElement('p');
+  counter.textContent = cat.count;
+
+  var img = document.createElement('img');
+  img.src = cat.img;
+  img.className = 'imgContainer';
+
+  //Click the image to increase the counter for the cat
+
+  img.addEventListener('click', (function(catCopy) {
+        return function() {
+            catCopy.count++;
+            console.log(catCopy.name + ': ' + catCopy.count);
+        };
+    })(cat));
+
+  //Append all the cats
+
+    document.getElementById('cat-list').appendChild(list);
+    document.getElementById('cat-picture').appendChild(elem);
+    document.getElementById(cat.name).appendChild(counter);
+    document.getElementById(cat.name).appendChild(img);
+
+  }
